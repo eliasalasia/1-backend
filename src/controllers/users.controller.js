df2f1017f8497f54ec838b039c3e40c7a951ecb1
ELIAS <elias.alasia.moreno@gmail.com>
2024-07-04 12:59:53 -0500

    actualizando users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,9 +3,9 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 
-// Obtener todos los usuarios
+// se obtiene todos los usuarios
 export const indexUsers = async (req, res) => {
-    // Obtener el token del encabezado de la solicitud
+    // Obtener el token del encabezado de la solicitud.
     const token = req.headers.authorization?.split(' ')[1];
 
     try {
@@ -14,23 +14,23 @@ export const indexUsers = async (req, res) => {
             return res.status(401).json({ message: 'Unauthorized' });
         }
 
-        // Decodificar el token para obtener el id y el rol del usuario
+        // Decodificar el token para obtener el id y el rol del usuario.
         const decoded = jwt.verify(token, 'secretkey');
-        const { id, role, nivel } = decoded; // Añadir nivel aquí
+        const { id, role, nivel } = decoded; 
 
-        // Verificar el rol para determinar el acceso
+        // Verificar el rol para determinar el acceso.
         if (role === 'teacher') {
-            // Si el usuario es un teacher, obtener todos los estudiantes con el mismo nivel
+            // Si el usuario es un "teacher", obtiene todos los estudiantes con el mismo nivel.
             const students = await User.find(
-                { role: 'student', nivel: nivel }, // Filtrar por nivel
-                '_id name lastname email role videos nivel matricula' // Proyección de campos
+                { role: 'student', nivel: nivel }, // Filtrar por nivel.
+                '_id name lastname email role videos nivel matricula' // Proyección de campos.
             );
             res.json(students);
         } else if (role === 'student') {
-            // Si el usuario es un student, obtener solo su propio usuario
+            // Si el usuario es un "student", se obtenie solo su propio usuario.
             const user = await User.findById(
                 id,
-                '_id name lastname email role videos nivel matricula' // Proyección de campos
+                '_id name lastname email role videos nivel matricula' // lo que me muestra de repuesta.
             );
             if (!user) {
                 return res.status(404).json({ message: 'User not found' });
@@ -61,7 +61,6 @@ export const createUser = async (req, res) => {
             password: hashedPassword,
             role,
             nivel
-            // No es necesario pasar matricula aquí
         });
         await newUser.save();
         res.status(201).json({ message: 'User created' });
@@ -84,7 +83,7 @@ export const loginUser = async (req, res) => {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
         const token = jwt.sign(
-            { id: user._id, role: user.role, nivel: user.nivel }, // Añadir nivel aquí
+            { id: user._id, role: user.role, nivel: user.nivel }, 
             'secretkey', 
             { expiresIn: '1h' }
         );
