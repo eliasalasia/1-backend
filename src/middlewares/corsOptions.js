13784ba7a6354b56be146baac336c36a899c5809
ELIAS <elias.alasia.moreno@gmail.com>
2024-07-12 14:54:33 -0500

    arreglado el problema de CORS

--- src/middlewares/corsOptions.js
+++ src/middlewares/corsOptions.js
@@ -1,9 +1,11 @@
-
 const corsOptions = {
-  origin:  ['http://localhost:5173/', 'http://localhost:5174/'], // Permite solicitudes solo desde este origen
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174'
+  ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
-  
-export default corsOptions
+
+export default corsOptions;
